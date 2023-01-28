27f647752d8aa866e0ab0f41720b1c8dc7afc3e3
jakobudovic <ju8168@student.uni-lj.si>
2023-01-28 17:00:04 +0100

    Add mock main js

--- main.js
+++ main.js
@@ -1,1 +1,8 @@
-console.log("The test extension is up and running")
+console.log("The extension is up and running2 ");
+
+var images = document.getElementsByTagName('img')
+
+for (elt of images){
+   elt.src = `${browser.runtime.getURL("aa.png")}`;
+   elt.alt = 'an alt text'
+}
