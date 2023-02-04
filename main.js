6bbddd35e11c74ce9dba7b6f94089047495d36a6
jakobudovic <ju8168@student.uni-lj.si>
2023-02-04 01:21:51 +0100

    Beautify file

--- main.js
+++ main.js
@@ -2,93 +2,92 @@ function get_table_data(table) {
   // asks & bids : Quantity, size, bid/ask price
   // sales : date, time, size, sale price
   const table_arr = Array.from(table.rows).slice(1); // remove header
-  return table_arr.map(row => row.cells) 
-                  .map(row => Array.from(row))
-                  .map(row => row.flatMap(cell => cell.innerText))
+  return table_arr.map(row => row.cells)
+    .map(row => Array.from(row))
+    .map(row => row.flatMap(cell => cell.innerText))
 }
 
 function insert_chart(selector, timeSeriesData) {
-    console.log("timeSeriesData");
-    console.log(timeSeriesData);
+  console.log("timeSeriesData");
+  console.log(timeSeriesData);
 
-    if (timeSeriesData[0].length < 4) {
-        return; // if "asks" or "bids" views open, skip chart insertion
-    }
+  if (timeSeriesData[0].length < 4) {
+    return; // if "asks" or "bids" views open, skip chart insertion
+  }
 
-    const parentDiv = document.createElement('div');
-    parentDiv.style.padding = '0px 24px';
+  const parentDiv = document.createElement('div');
+  parentDiv.style.padding = '0px 24px';
 
-    const canvas = document.createElement('canvas');
-    canvas.id = "myChartJS";
-    parentDiv.appendChild(canvas);
+  const canvas = document.createElement('canvas');
+  canvas.id = "myChartJS";
+  parentDiv.appendChild(canvas);
 
-    // chakra-modal--body-27 or chakra-modal-11
-    document.querySelector("#chakra-modal-11").insertBefore(parentDiv, document.querySelector(selector));
-    var ctx = document.getElementById("myChartJS").getContext("2d");
+  // chakra-modal--body-27 or chakra-modal-11
+  document.querySelector("#chakra-modal-11").insertBefore(parentDiv, document.querySelector(selector));
+  var ctx = document.getElementById("myChartJS").getContext("2d");
 
-    // Format the time series data for Chart.js
-    var chartData = {
-        labels: timeSeriesData.map(item => new Date(`${item[0]} ${item[1]}`)),
-        datasets: [{
-            label: "Shoe price",
-            data: timeSeriesData.map(x => parseInt(x[3].slice(1))),
-            backgroundColor: "rgba(54, 162, 235, 0.2)",
-            borderColor: "rgba(54, 162, 235, 1)",
-            borderWidth: 3,
+  // Format the time series data for Chart.js
+  var chartData = {
+    labels: timeSeriesData.map(item => new Date(`${item[0]} ${item[1]}`)),
+    datasets: [{
+      label: "Shoe price",
+      data: timeSeriesData.map(x => parseInt(x[3].slice(1))),
+      backgroundColor: "rgba(54, 162, 235, 0.2)",
+      borderColor: "rgba(54, 162, 235, 1)",
+      borderWidth: 3,
+    }, ],
+  };
+
+  var chart = new Chart(ctx, {
+    type: "line",
+    data: chartData,
+    options: {
+      scales: {
+        x: {
+          type: 'time',
+          time: {
+            displayFormats: {
+              'minute': 'd MMM, h:m a'
             },
-        ],
-    };
-
-    var chart = new Chart(ctx, {
-        type: "line",
-        data: chartData,
-        options: {
-            scales: {
-              x: {
-                type: 'time',
-                time: {
-                  displayFormats: {
-                    'minute': 'd MMM, h:m a'
-                  },
-                  tooltipFormat: "d MMM, H:mm a",
-                },
-                ticks: {
-                  maxTicksLimit: 5,
-                },
-              }
+            tooltipFormat: "d MMM, H:mm a",
+          },
+          ticks: {
+            maxTicksLimit: 5,
+          },
+        }
+      },
+      elements: {
+        point: {
+          pointBorderWidth: 0.5, // less background at chart points
+        },
+        line: {
+          tension: 0.05
+        }
+      },
+      plugins: {
+        tooltip: {
+          callbacks: {
+            label: function (tooltipItem) {
+              return `${tooltipItem.dataset.label}: ${timeSeriesData[tooltipItem.dataIndex][3]}`
             },
-            elements: {
-                point: {
-                    pointBorderWidth: 0.5, // less background at chart points
-                },
-                line: {
-                    tension: 0.05
-                }
-            },
-            plugins: {
-                tooltip: {
-                  callbacks: {
-                    label: function(tooltipItem) {
-                      return `${tooltipItem.dataset.label}: ${timeSeriesData[tooltipItem.dataIndex][3]}`
-                    },
-                    footer: function(tooltipItems) {
-                      tooltipItem = tooltipItems[0];
-                      return `Size: ${timeSeriesData[tooltipItem.dataIndex][2]}`
-                    }
-                  }
-                },
-              }
+            footer: function (tooltipItems) {
+              tooltipItem = tooltipItems[0];
+              return `Size: ${timeSeriesData[tooltipItem.dataIndex][2]}`
+            }
           }
-    });
+        },
+      }
+    }
+  });
 }
 
 function display_chart(table) {
-    var table_data = get_table_data(table);
-    insert_chart("#chakra-modal--body-11", table_data);
+  var table_data = get_table_data(table);
+  insert_chart("#chakra-modal--body-11", table_data);
 }
 
 function remove_cookie_product_visits(cookie_name) {
-    document.cookie = `${cookie_name}=1; expires=Sun, 31 Dec 2033 12:00:00 UTC; path=/;`;
+  document.cookie = `${cookie_name}=1; expires=Sun, 31 Dec 2033 12:00:00 UTC; path=/;`;
 }
 
 const targetNode = document.body;
@@ -100,8 +99,8 @@ const observer = new MutationObserver((mutationsList) => {
           const tableNodes = node.querySelectorAll('.css-1ki54i');
           if (tableNodes.length) {
             for (const tableNode of tableNodes) {
-                display_chart(tableNode);
-                return;
+              display_chart(tableNode);
+              return;
             }
           }
         }
@@ -110,10 +109,13 @@ const observer = new MutationObserver((mutationsList) => {
   }
 });
 
-observer.observe(targetNode, { childList: true, subtree: true });
+observer.observe(targetNode, {
+  childList: true,
+  subtree: true
+});
 
-window.addEventListener('load', function load(e){
-    window.removeEventListener('load', load, false);
-    remove_cookie_product_visits("stockx_product_visits")
-    this.setTimeout(() => console.log("ran with delay"), 1000);
+window.addEventListener('load', function load(e) {
+  window.removeEventListener('load', load, false);
+  remove_cookie_product_visits("stockx_product_visits")
+  this.setTimeout(() => console.log("ran with delay"), 1000);
 }, false);
